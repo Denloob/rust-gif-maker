c463e51dfac9164e80430d93c4061c24173ea3f4
Denloob <magtop@denloob.tk>
2023-07-04 19:54:31 +0300

    Add input_valid function

--- src/input.rs
+++ src/input.rs
@@ -10,3 +10,32 @@ pub fn input<T: FromStr>() -> Result<T, T::Err> {
 
     string.trim().parse()
 }
+
+/// [input]s T from stdin until the input is valid.
+///
+/// Each time [input] fails, prints so to the console.
+///
+/// # Examples
+///
+/// ```no_run
+/// println!("Please enter a number: ");
+/// let num: i32 = input_valid();
+/// println!("Your number is: {}", num);
+/// ```
+///
+/// Example for what the user could see:
+/// ```text
+/// Please enter a number:
+/// abc
+/// Invalid input. Please try again:
+/// 3
+/// Your number is: 3
+/// ```
+pub fn input_valid<T: FromStr>() -> T {
+    loop {
+        match input::<T>() {
+            Ok(value) => return value,
+            Err(_) => println!("Invalid input. Please try again:"),
+        }
+    }
+}
